a9bf044681cac400346f4d58d607306920cf7ac2
amine-by <aminebenyedder@outlook.com>
2026-05-08 12:17:37 +0100

    refactor: simplify exports and add types to public API

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,5 @@
 import GameEngine from './GameEngine';
-import {
-  configure,
-  pause,
-  resume,
-  spawn,
-  despawn,
-  update,
-} from './GameMethods';
 
-export { GameEngine, configure, pause, resume, spawn, despawn, update };
+export * from './GameMethods';
+export * from './types';
+export { GameEngine };
